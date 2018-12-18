8f84df780c23720edfaa2584cb28c7cf926c4265
Yanelis Serrano Taylor <yaneliserranotaylor@gmail.com>
2018-12-18 22:36:26 +0100

    Update process_data.scala

--- process_data.scala
+++ process_data.scala
@@ -1,1 +1,10 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib
 
+val DATA = PATH + FILE
+
+val lines = sc.textFile(DATA)
+val nonEmpty = lines.filter(_.nonEmpty)
+val parsed = nonEmpty.map(line => line.split(","))
+
+parsed.first
